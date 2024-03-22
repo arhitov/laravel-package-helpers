0ee6b0b62c7f3e1cd7d62f3b89fac5c7d8413cf4
clgsru <clgsru@gmail.com>
2024-03-22 20:10:48 +0300

    Using inline closure

--- src/Migrations/PublishesMigrationsTrait.php
+++ src/Migrations/PublishesMigrationsTrait.php
@@ -19,15 +19,18 @@ trait PublishesMigrationsTrait
     protected function registerMigrations(string $directory): void
     {
         if ($this->app->runningInConsole()) {
-            $generator = function(string $directory): Generator {
-                foreach ($this->app->make('files')->allFiles($directory) as $file) {
-                    yield $file->getPathname() => $this->app->databasePath(
-                        'migrations/' . now()->format('Y_m_d_His') . Str::after($file->getFilename(), '00_00_00_000000')
-                    );
-                }
-            };
-
-            $this->publishes(iterator_to_array($generator($directory)), 'migrations');
+            $this->publishes(
+                iterator_to_array(
+                    (function() use ($directory): Generator {
+                        foreach ($this->app->make('files')->allFiles($directory) as $file) {
+                            yield $file->getPathname() => $this->app->databasePath(
+                                'migrations/' . now()->format('Y_m_d_His') . Str::after($file->getFilename(), '00_00_00_000000')
+                            );
+                        }
+                    })()
+                ),
+                'migrations'
+            );
         }
     }
 }
