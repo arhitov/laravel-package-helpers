7b9b4ba4484a2843b070e10922b895220f48c9d0
clgsru <clgsru@gmail.com>
2024-04-22 13:19:51 +0300

    PublishesMigrations now does not generate migration file names by default.

--- src/Migrations/PublishesMigrationsTrait.php
+++ src/Migrations/PublishesMigrationsTrait.php
@@ -12,17 +12,22 @@ trait PublishesMigrationsTrait
      *
      * @param string $directory
      * @param string $tabName
+     * @param bool $staticName Use static name file
      * @return void
      */
-    protected function registerMigrations(string $directory, string $tabName = 'migrations'): void
+    protected function registerMigrations(string $directory, string $tabName = 'migrations', bool $staticName = true): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes(
                 iterator_to_array(
-                    (function() use ($directory): Generator {
+                    (function() use ($directory, $staticName): Generator {
                         foreach ($this->app->make('files')->allFiles($directory) as $file) {
                             yield $file->getPathname() => $this->app->databasePath(
-                                'migrations/' . now()->format('Y_m_d_His') . Str::after($file->getFilename(), '00_00_00_000000')
+                                'migrations/' . (
+                                    $staticName
+                                        ? $file->getFilename()
+                                        : now()->format('Y_m_d_His') . Str::after($file->getFilename(), '00_00_00_000000')
+                                )
                             );
                         }
                     })()
